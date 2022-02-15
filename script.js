16befcd3b2022a4d0a11b0fadf352e1afbd04766
doeixd <doeixd@gmail.com>
2022-02-15 13:49:22 -0500

    2 files modified

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var State = { ...defaultGameState, ...defaultUserState };
 localStorageGameState()?.persist && update(localStorageGameState())
 
 let possibleWords;
-var tiles = getTiles();
+
 
 // --------------------------------------------------------
 // RUNTIME:
@@ -244,7 +244,7 @@ async function paint(oldGameState, newGameState) {
 
     const { letter: oldLetter, state: oldState } = oldGameState.gameBoard[idx];
     if (oldLetter == letter && state == oldState) return;
-    
+    const tiles = getTiles();
     tiles[idx].innerText = letter ?? '';
     tiles[idx].classList = '';
     tiles[idx].classList.add(state);
