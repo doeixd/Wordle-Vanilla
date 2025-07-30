f6b94d06554ffebd55695cddf20ed6b5e227f456
Patrick G <doeixd@gmail.com>
2025-07-30 13:44:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -341,7 +341,7 @@ async function animateSumbittedRow(oldState, newState) {
         ],
         {
           duration: 600,
-          delay:   (idx * ( 300 - ( idx * 1 ) )) + 50,
+          delay:   (idx *  300 ) + 50,
           easing: 'ease-in-out',
         }
       ).onfinish = async (_) => {
